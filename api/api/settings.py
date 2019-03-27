8a88ba573625179985e8ac568609fb9f3af43a61
RobertoKennedy <32366581+RobertoKennedy@users.noreply.github.com>
2019-03-27 15:43:31 -0300

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -30,17 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-	'rest_framework',
-	'core',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
+INSTALLED_APPS = ['rest_framework','core','django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
